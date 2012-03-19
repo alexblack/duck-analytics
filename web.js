86f7c423b9a584d35d11d5b55b01b7ae6f885660
Alex Black <alex@alexblack.ca>
2012-03-18 22:09:30 -0400

    add readme

--- web.js
+++ web.js
@@ -3,8 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.use(express.static('./public'));
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/tracker.gif', function(request, response) {
+  var ua = request.param('ua', null);
+  var referrer = request.param('referrer', null);
+  var url = request.param('url', null);
+  
+  response.sendfile('./public/images/tracker.gif');
 });
 
 var port = process.env.PORT || 3000;
